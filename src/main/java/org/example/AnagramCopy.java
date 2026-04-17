4b67adac25fe9a7ea85455be279ec2afbd6f1df0
raj02kumar94 <raj02kumar94@yahoo.com>
2026-04-17 08:59:19 +0530

    adding new changes from Branch1

---
+++ src/main/java/org/example/AnagramCopy.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.*;
+
+public class AnagramCopy {
+
+    static void main() {
+
+        String strA = "Geeks";
+        String strB = "kseg";
+
+        strA = strA.toLowerCase();
+        strB = strB.toLowerCase();
+
+        if(strA.length()==strB.length()){
+
+            char[] ch1 = strA.toCharArray();
+            char[] ch2 = strB.toCharArray();
+
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+
+            boolean result = Arrays.equals(ch1, ch2);
+            if(result){
+                System.out.println(strA+" "+strB + " is anagram");
+
+            }
+        }else{
+
+            System.out.println(strA+" not equal "+strB);
+        }
+
+        Map<String, Integer> hMap = new LinkedHashMap<>();
+
+        hMap.put("David",95);
+        hMap.put("Jane",80);
+        hMap.put("Mary",97);
+        hMap.put("Lisa",78);
+        hMap.put("Dino",65);
+
+        System.out.println(hMap);
+
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(hMap.entrySet());
+        entryList.sort((a,b)-> b.getValue().compareTo(a.getValue()));
+
+
+        for(Map.Entry<String, Integer> entry: entryList){
+
+            System.out.println(entry.getKey() + ": "+ entry.getValue());
+        }
+
+
+    }
+}
