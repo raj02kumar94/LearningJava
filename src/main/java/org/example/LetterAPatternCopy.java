9e24c0972b19a5b2b7d5dd6882b144f29566c152
raj02kumar94 <raj02kumar94@yahoo.com>
2026-04-17 08:42:44 +0530

    Adding new file in Branch2

---
+++ src/main/java/org/example/LetterAPatternCopy.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class LetterAPatternCopy {
+
+    public static void main(String[] args) {
+        int height = 7; // Height of the letter
+        int width = 5;  // Width of the letter
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // Logic for printing the 'A' shape
+                if ((i == 0 && j > 0 && j < width - 1) || // Top bar (curved)
+                        (i == height / 2) ||                  // Middle bar
+                        (j == 0 && i > 0) ||                  // Left side
+                        (j == width - 1 && i > 0)) {          // Right side
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // Move to next line
+        }
+    }
+}
